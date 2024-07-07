ae5f2e7894641d96a4b0d7dad379b084c7c38fe1
kaoru <nsaidov530@gmail.com>
2024-07-07 19:16:13 +0300

    Добавил вывод из бд карточек!

--- app/src/main/java/com/example/kartochki/MainActivity.kt
+++ app/src/main/java/com/example/kartochki/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.example.kartochki
 
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.ProgressBar
+import android.widget.ScrollView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
+import com.example.kartochki.Models.Card
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var progressBar: ProgressBar
+    private lateinit var container: LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +32,54 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        progressBar = findViewById(R.id.progressBar)
+        container = findViewById(R.id.Container)
+
+        fetchDataFromFirebase()
+    }
+
+    private fun fetchDataFromFirebase() {
+        val database = FirebaseDatabase.getInstance().getReference("cards")
+        progressBar.visibility = ProgressBar.VISIBLE
+
+        database.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                progressBar.visibility = ProgressBar.GONE
+                container.removeAllViews()
+                for (cardSnapshot in snapshot.children) {
+                    val card = cardSnapshot.getValue(Card::class.java)
+                    if (card != null) {
+                        addCardToContainer(card)
+                    }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                progressBar.visibility = ProgressBar.GONE
+                Log.e("MainActivity", "Failed to read data from Firebase", error.toException())
+            }
+        })
+    }
+
+    private fun addCardToContainer(card: Card) {
+        val inflater = LayoutInflater.from(this)
+        val cardView = inflater.inflate(R.layout.single_card, container, false)
+
+        val cardName = cardView.findViewById<TextView>(R.id.card_name)
+        val cardImage = cardView.findViewById<ImageView>(R.id.image_card)
+        val peopleCard = cardView.findViewById<TextView>(R.id.people_card)
+        val cubeCard = cardView.findViewById<TextView>(R.id.cube_card)
+        val timeCard = cardView.findViewById<TextView>(R.id.time_card)
+
+        cardName.text = card.name
+        Glide.with(this)
+            .load(card.image)
+            .into(cardImage)
+        peopleCard.text = card.people
+        cubeCard.text = card.cube
+        timeCard.text = card.time
+
+        container.addView(cardView)
     }
 }
---
+++ app/src/main/java/com/example/kartochki/Models/Card.kt
@@ -0,0 +1,11 @@
+package com.example.kartochki.Models
+
+data class Card(
+    var id: String? = null,
+    val name: String? = null,
+    val image: String? = null,
+    val people: String? = null,
+    val cube: String? = null,
+    val time: String? = null,
+    val opisanie: String? = null
+)
